9778177bcb26dc8ced0fd00bc356758a0d9d0a9f
Carlos Rudyelissom <carlos.devunity@outlook.com>
2022-12-03 14:47:23 -0300

    configure editor settings

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -1,9 +1,11 @@
+using System.Collections.ObjectModel;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
