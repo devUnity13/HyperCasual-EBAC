c9c6c0e811035c325cc8a68e9cf3b634e3a3055c
Carlos Rudyelissom <carlos.devunity@outlook.com>
2022-12-03 19:05:30 -0300

    update system

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -1,20 +1,8 @@
-using System.Collections.ObjectModel;
-using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
