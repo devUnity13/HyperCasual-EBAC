6c9911a7fccd376794233d5e928f30dce19c1fdc
Carlos Rudyelissom <carlos.devunity@outlook.com>
2022-12-03 19:08:23 -0300

    implement touch controller and lerp system

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -1,8 +1,22 @@
+using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
 public class TouchController : MonoBehaviour
 {
-    
+    public Vector2 pastPosition;
+    public float velocity = 1f;
+    private void Update() 
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Move(Input.mousePosition.x - pastPosition.x);
+        }
+        pastPosition = Input.mousePosition;    
+    }
+
+    private void Move(float speed)
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed * velocity;
+    }
 }
