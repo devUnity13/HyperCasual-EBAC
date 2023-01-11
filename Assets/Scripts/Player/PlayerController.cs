92f90cd4f070fb03fdb2a92485c14e8f9c1ed1d4
Carlos Rudyelissom <112135015+devUnity13@users.noreply.github.com>
2023-01-10 21:49:58 -0300

    First Exercices of Hyper Casual

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Lerp")]
+    //Variaveis publicas
+    public Transform target;
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float lerpSpeed = 1f;
+    public string tagEnemy = "Enemy";
+    public string TagFinish = "Finish";
+    public GameObject[] Screen;
 
-    // Update is called once per frame
+    //Variaveis privadas
+    private Vector3 _pos;
+    private bool _canRun = false;
+
     void Update()
     {
+        if (!_canRun) return;
+
+        _pos = target.position;
+        _pos.y = transform.position.y;
+        _pos.z = transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
         transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == tagEnemy)
+        {
+            _canRun = false;
+            EndGame();
+            Debug.Log("GameOver!");
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.transform.tag == TagFinish)
+        {
+            _canRun = false;
+            EndGame();
+            Debug.Log("Você Venceu!");
+        }
+    }
+    public void StartGame()
+    {
+        _canRun = true;
+        Screen[0].SetActive(false);
+        if (Screen[1])
+        {
+            Screen[1].SetActive(false);
+        }
+
+    }
+
+    public void EndGame()
+    {
+        Screen[1].SetActive(true);
     }
 }
--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -6,13 +6,24 @@ public class TouchController : MonoBehaviour
 {
     public Vector2 pastPosition;
     public float velocity = 1f;
+    private float _sideLimit = 4.5f;
+
     private void Update() 
     {
         if(Input.GetMouseButton(0))
         {
             Move(Input.mousePosition.x - pastPosition.x);
         }
-        pastPosition = Input.mousePosition;    
+        pastPosition = Input.mousePosition;
+
+        if(transform.position.x > _sideLimit)
+        {
+            transform.position = new Vector3(_sideLimit,0,0);
+        }
+        else if(transform.position.x < -_sideLimit)
+        {
+            transform.position = new Vector3(-_sideLimit,0,0);
+        }
     }
 
     private void Move(float speed)
